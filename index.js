7d4b28b7ef3c428baf244147718bfc673732e57a
Muhammad Riza <riza.riza69@gmail.com>
2019-04-05 09:48:50 +0700

    project javascript object

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+console.log({
+  name: 'Alpha',
+  age: 100,
+  superPower: true
+})
+console.log({
+  name: 'Alpha',
+  age: 100,
+  superPower: true
+})
+
+const hero = {
+  name: 'Alpha',
+  age: 100,
+  superPower: true
+}
+
+console.log(hero)
+console.log(hero['name'])
+console.log(hero.age)
+console.log(hero.superPower)
+
+// alert(hero)
+// alert(hero['name'])
+// alert(hero.age)
+// alert(hero.superPower)
+
+const student = {
+  name: 'dudu',
+  age: 15,
+  score: 70
+}
+
+console.log(student);
+console.log(student['name']);
+console.log(student.age);
+console.log(student['score']);
